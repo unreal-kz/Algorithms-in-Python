158d95cecb8e6ee1ffa2c4d70c179b8dd91a93c9
unreal-kz <ukibas.b@gmail.com>
2020-10-18 16:00:18 +0000

    Recursion File has been modified

--- RecursionUseCase.py
+++ RecursionUseCase.py
@@ -1,6 +1,6 @@
 # Find the length of the string
 
-sample = "Hello, world!!!"
+sample = "Hello"
 
 def find_len_recursively(my_string):
 
@@ -8,4 +8,16 @@ def find_len_recursively(my_string):
         return 0
     return 1 + find_len_recursively(my_string[1:])
 
-print (find_len_recursively(sample))
+# print (find_len_recursively(sample))
+
+
+# Reverse the string
+
+def reverse(s): 
+    if len(s) == 0: 
+        return s 
+    else: 
+        return reverse(s[1:]) + s[0]
+
+# print(sample[0:])
+# print(reverse(sample))
--- test.py
+++
@@ -1,9 +0,0 @@
-#Function declaration
-def func_name():
-    name = input ("Enter your name: ")
-    age = input ("Enter your age: ")
-    print (f"Hello {name}, you are {age} years old :)") 
-
-#Function call
-func_name()
-
